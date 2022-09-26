f8d945ca691b09c86e8780965529490b6e23e11d
Ceed Jennelle Lorenzo <bg201801625@wmsu.edu.ph>
2022-09-26 18:17:41 +0800

    added a source in the listOfProjects

--- src/components/Projects/listProjects.js
+++ src/components/Projects/listProjects.js
@@ -4,31 +4,23 @@ export const ProjectList = [
     title: "E-commerce Project",
     description:
       "An E-commerce website built using React, Redux, and Firebase with user and admin modules.",
+    source: "Github",
     link: "https://github.com/LorenzoCeedJennelle/my-app--AJC-PIZZA-",
+    screenshots: "",
     liveView: "https://ajc-pizza.web.app/",
     tags: ["React", "Firebase", "React Sass", "Redux"],
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVoPtx7AlK3nOEfMYMerCZE3ZWiMTtZWNLiw&usqp=CAU",
   },
-
   {
     id: 1,
     title: "Lines Hub Online Sales and Inventory System",
     description:
-      "Designed, developed, and built this system this online sales and inventory system for our OJT (On-the-Job) training.",
+      "Designed, developed, and built this system this client-based online sales and inventory system for our OJT (On-the-Job) training.",
     tags: ["React", "Firebase", "Material-UI"],
-    link: "https://github.com/LorenzoCeedJennelle/Online-Sales-and-Inventory-System",
+    source: "Screenshot",
+    link: "  https://drive.google.com/uc?id=1i7ghT87tMgcqESsl3pxtF4_G7--frB8K",
     liveView: "https://lines-hub.web.app/",
     image: "https://drive.google.com/uc?id=1kbWY6-ei08BLpDQ_A6aEybn3CDSgoQiV",
   },
-  // {
-  //   id: 2,
-  //   title: "Vaccination Tracking System",
-  //   description:
-  //     "Designed, developed, and built this Vaccination Tracking System with user and admin modules",
-  //   tags: ["React", "Firebase", "Material-UI", "CSS"],
-  //   liveView: "https://alpha-testing-fd696.web.app/",
-  //   link: "https://github.com/LorenzoCeedJennelle/VaccinationTracking",
-  //   image: "https://drive.google.com/uc?id=1dGWnrpEk6h-2P5rLimDmP-THhzd0yenE",
-  // },
 ];
