a8ff0e6b9401b1ee97ec26333cd8eb25ef7df494
Ceed Jennelle Lorenzo <bg201801625@wmsu.edu.ph>
2023-05-23 15:16:41 +0800

    added the schoool project [vaccine tracking system]

--- src/components/Projects/listProjects.js
+++ src/components/Projects/listProjects.js
@@ -16,13 +16,28 @@ export const ProjectList = [
     id: 1,
     title: "Lines Hub Online Sales and Inventory System",
     description:
-      "Designed, developed, and built this system this client-based online sales and inventory system for our OJT (On-the-Job) training.",
+      "Designed, developed, and built this client-based online sales and inventory system for our OJT (On-the-Job) training.",
     tags: ["React", "Firebase", "Material-UI"],
     source: "Screenshot",
     link: "  https://drive.google.com/uc?id=1NMQkIprBZI8v7dwGXA_t4rKR_VLBf68a",
     liveView: "https://lines-hub.web.app/",
     image: "https://drive.google.com/uc?id=1kbWY6-ei08BLpDQ_A6aEybn3CDSgoQiV",
   },
+  {
+    id: 2,
+    title: "Vaccination Tracking System",
+    description:
+      "Designed, developed, and built this vaccination tracking system as part of the school project.",
+    tags: ["React", "Firebase", "Material-UI"],
+    source: "Screenshot",
+    link: "https://drive.google.com/uc?id=127KJSHenEl-S7VZsNEunPwYt8F1B0--n",
+    liveView: "https://alpha-testing-fd696.web.app/",
+    image: "https://drive.google.com/uc?id=1dGWnrpEk6h-2P5rLimDmP-THhzd0yenE",
+  },
 ];
 
 // https://drive.google.com/file/d/1NMQkIprBZI8v7dwGXA_t4rKR_VLBf68a/view?usp=sharing
+
+//https://drive.google.com/file/d/1dGWnrpEk6h-2P5rLimDmP-THhzd0yenE/view?usp=sharing <-- google drive vaccine tracking picture
+
+//https://drive.google.com/file/d/127KJSHenEl-S7VZsNEunPwYt8F1B0--n/view?usp=sharing <-- google drive ppt for vaccine tracking system
